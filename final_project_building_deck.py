d263232bde3f72295088b68951696099ed668e34
Kit M <kemcdo@nyu.edu>
2018-07-31 19:35:35 -0400

    Change DECK_DICT key from "actual_value" to "value"

--- final_project_building_deck.py
+++ final_project_building_deck.py
@@ -2,69 +2,69 @@ import random
 from time import sleep
 
 
-suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']*13
-values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']*4
-trial_deck = {}
-for i in range(1, 53):
-	trial_deck[i] = {
-						'card': values[i-1] + ' of ' + suits[i-1],
-						'actual_value': []
-
-					}
+# suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']*13
+# values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']*4
+# trial_deck = {}
+# for i in range(1, 53):
+# 	trial_deck[i] = {
+# 						'card': values[i-1] + ' of ' + suits[i-1],
+# 						'value': []
+#
+# 					}
 
 DECK_DICT = {
-				1: {'card': 'A ♣', 'actual_value': [1, 11]},
-				2: {'card': '2 ♥', 'actual_value': [2]},
-				3: {'card': '3 ♦', 'actual_value': [3]},
-				4: {'card': '4 ♠', 'actual_value': [4]},
-				5: {'card': '5 ♣', 'actual_value': [5]},
-				6: {'card': '6 ♥', 'actual_value': [6]},
-				7: {'card': '7 ♦', 'actual_value': [7]},
-				8: {'card': '8 ♠', 'actual_value': [8]},
-				9: {'card': '9 ♣', 'actual_value': [9]},
-				10: {'card': '10 ♥', 'actual_value': [10]},
-				11: {'card': 'J ♦', 'actual_value': [10]},
-				12: {'card': 'Q ♠', 'actual_value': [10]},
-				13: {'card': 'K ♣', 'actual_value': [10]},
-				14: {'card': 'A ♥', 'actual_value': [1, 11]},
-				15: {'card': '2 ♦', 'actual_value': [2]},
-				16: {'card': '3 ♠', 'actual_value': [3]},
-				17: {'card': '4 ♣', 'actual_value': [4]},
-				18: {'card': '5 ♥', 'actual_value': [5]},
-				19: {'card': '6 ♦', 'actual_value': [6]},
-				20: {'card': '7 ♠', 'actual_value': [7]},
-				21: {'card': '8 ♣', 'actual_value': [8]},
-				22: {'card': '9 ♥', 'actual_value': [9]},
-				23: {'card': '10 ♦', 'actual_value': [10]},
-				24: {'card': 'J ♠', 'actual_value': [10]},
-				25: {'card': 'Q ♣', 'actual_value': [10]},
-				26: {'card': 'K ♥', 'actual_value': [10]},
-				27: {'card': 'A ♦', 'actual_value': [1, 11]},
-				28: {'card': '2 ♠', 'actual_value': [2]},
-				29: {'card': '3 ♣', 'actual_value': [3]},
-				30: {'card': '4 ♥', 'actual_value': [4]},
-				31: {'card': '5 ♦', 'actual_value': [5]},
-				32: {'card': '6 ♠', 'actual_value': [6]},
-				33: {'card': '7 ♣', 'actual_value': [7]},
-				34: {'card': '8 ♥', 'actual_value': [8]},
-				35: {'card': '9 ♦', 'actual_value': [9]},
-				36: {'card': '10 ♠', 'actual_value': [10]},
-				37: {'card': 'J ♣', 'actual_value': [10]},
-				38: {'card': 'Q ♥', 'actual_value': [10]},
-				39: {'card': 'K ♦', 'actual_value': [10]},
-				40: {'card': 'A ♠', 'actual_value': [1, 11]},
-				41: {'card': '2 ♣', 'actual_value': [2]},
-				42: {'card': '3 ♥', 'actual_value': [3]},
-				43: {'card': '4 ♦', 'actual_value': [4]},
-				44: {'card': '5 ♠', 'actual_value': [5]},
-				45: {'card': '6 ♣', 'actual_value': [6]},
-				46: {'card': '7 ♥', 'actual_value': [7]},
-				47: {'card': '8 ♦', 'actual_value': [8]},
-				48: {'card': '9 ♠', 'actual_value': [9]},
-				49: {'card': '10 ♣', 'actual_value': [10]},
-				50: {'card': 'J ♥', 'actual_value': [10]},
-				51: {'card': 'Q ♦', 'actual_value': [10]},
-				52: {'card': 'K ♠', 'actual_value': [10]}}
+				1: {'card': 'A ♣', 'value': [1, 11]},
+				2: {'card': '2 ♥', 'value': [2]},
+				3: {'card': '3 ♦', 'value': [3]},
+				4: {'card': '4 ♠', 'value': [4]},
+				5: {'card': '5 ♣', 'value': [5]},
+				6: {'card': '6 ♥', 'value': [6]},
+				7: {'card': '7 ♦', 'value': [7]},
+				8: {'card': '8 ♠', 'value': [8]},
+				9: {'card': '9 ♣', 'value': [9]},
+				10: {'card': '10 ♥', 'value': [10]},
+				11: {'card': 'J ♦', 'value': [10]},
+				12: {'card': 'Q ♠', 'value': [10]},
+				13: {'card': 'K ♣', 'value': [10]},
+				14: {'card': 'A ♥', 'value': [1, 11]},
+				15: {'card': '2 ♦', 'value': [2]},
+				16: {'card': '3 ♠', 'value': [3]},
+				17: {'card': '4 ♣', 'value': [4]},
+				18: {'card': '5 ♥', 'value': [5]},
+				19: {'card': '6 ♦', 'value': [6]},
+				20: {'card': '7 ♠', 'value': [7]},
+				21: {'card': '8 ♣', 'value': [8]},
+				22: {'card': '9 ♥', 'value': [9]},
+				23: {'card': '10 ♦', 'value': [10]},
+				24: {'card': 'J ♠', 'value': [10]},
+				25: {'card': 'Q ♣', 'value': [10]},
+				26: {'card': 'K ♥', 'value': [10]},
+				27: {'card': 'A ♦', 'value': [1, 11]},
+				28: {'card': '2 ♠', 'value': [2]},
+				29: {'card': '3 ♣', 'value': [3]},
+				30: {'card': '4 ♥', 'value': [4]},
+				31: {'card': '5 ♦', 'value': [5]},
+				32: {'card': '6 ♠', 'value': [6]},
+				33: {'card': '7 ♣', 'value': [7]},
+				34: {'card': '8 ♥', 'value': [8]},
+				35: {'card': '9 ♦', 'value': [9]},
+				36: {'card': '10 ♠', 'value': [10]},
+				37: {'card': 'J ♣', 'value': [10]},
+				38: {'card': 'Q ♥', 'value': [10]},
+				39: {'card': 'K ♦', 'value': [10]},
+				40: {'card': 'A ♠', 'value': [1, 11]},
+				41: {'card': '2 ♣', 'value': [2]},
+				42: {'card': '3 ♥', 'value': [3]},
+				43: {'card': '4 ♦', 'value': [4]},
+				44: {'card': '5 ♠', 'value': [5]},
+				45: {'card': '6 ♣', 'value': [6]},
+				46: {'card': '7 ♥', 'value': [7]},
+				47: {'card': '8 ♦', 'value': [8]},
+				48: {'card': '9 ♠', 'value': [9]},
+				49: {'card': '10 ♣', 'value': [10]},
+				50: {'card': 'J ♥', 'value': [10]},
+				51: {'card': 'Q ♦', 'value': [10]},
+				52: {'card': 'K ♠', 'value': [10]}}
 
 user_list = [
 			{
@@ -123,23 +123,23 @@ def get_dealer_two_card_sum(user_list, deck):
 	"""Getting sum of dealer's two cards, and returning their total along with
 	length of the first and second card (to check which is an ace)."""
 	total = 0
-	card_1_len = len(DECK_DICT[user_list[0]['current_hand'][0]]['actual_value']) # check for Ace
-	card_2_len = len(DECK_DICT[user_list[0]['current_hand'][1]]['actual_value']) # check for Ace
+	card_1_len = len(DECK_DICT[user_list[0]['current_hand'][0]]['value']) # check for Ace
+	card_2_len = len(DECK_DICT[user_list[0]['current_hand'][1]]['value']) # check for Ace
 	if card_1_len != 2 and card_2_len != 2: # neither is an ace
-		total += DECK_DICT[user_list[0]['current_hand'][0]]['actual_value'][0] + DECK_DICT[user_list[0]['current_hand'][1]]['actual_value'][0] # sum of both cards
+		total += DECK_DICT[user_list[0]['current_hand'][0]]['value'][0] + DECK_DICT[user_list[0]['current_hand'][1]]['value'][0] # sum of both cards
 	elif card_1_len == 2 and card_2_len == 2: # both are aces - very rare
 		total = 12
 	elif card_1_len == 2: # card 1 is an Ace
-		total += DECK_DICT[user_list[0]['current_hand'][0]]['actual_value'][1] + DECK_DICT[user_list[0]['current_hand'][1]]['actual_value'][0] # sum of both cards (where first is Ace = 11)
+		total += DECK_DICT[user_list[0]['current_hand'][0]]['value'][1] + DECK_DICT[user_list[0]['current_hand'][1]]['value'][0] # sum of both cards (where first is Ace = 11)
 	elif card_2_len == 2: # card 2 is an Ace
-		total += DECK_DICT[user_list[0]['current_hand'][0]]['actual_value'][0] + DECK_DICT[user_list[0]['current_hand'][1]]['actual_value'][1] # sum of both cards (where second is Ace = 11)
+		total += DECK_DICT[user_list[0]['current_hand'][0]]['value'][0] + DECK_DICT[user_list[0]['current_hand'][1]]['value'][1] # sum of both cards (where second is Ace = 11)
 
 	return total, card_1_len, card_2_len
 
 def check_if_ace(card):
 	"""just checking if current card is an ace. This is to save on
 	code redundancy (since we have to check if it is an ace often)."""
-	if len(DECK_DICT[card]['actual_value']) == 2:
+	if len(DECK_DICT[card]['value']) == 2:
 		return True
 	else:
 		return False
@@ -159,7 +159,7 @@ def less_than_17(user_list, deck, total, card_1_len, card_2_len):
 				total = 7
 				total = soft_17(user_list, deck, total)
 			else:
-				total += DECK_DICT[curr_card]['actual_value'][0]
+				total += DECK_DICT[curr_card]['value'][0]
 
 			print(print_dealer_cards(running_total=total)) # print dealer cards after getting next card
 
@@ -171,7 +171,7 @@ def less_than_17(user_list, deck, total, card_1_len, card_2_len):
 		while True:
 			curr_card = deck.pop() # take card from deck
 			user_list[0]['current_hand'].append(curr_card) # add to dealer's current hand
-			curr_card_value = DECK_DICT[curr_card]['actual_value'][0] # Ace will always = 1 in this case
+			curr_card_value = DECK_DICT[curr_card]['value'][0] # Ace will always = 1 in this case
 
 			temp_total = total + curr_card_value
 			if temp_total >= 22 and soft_card: # check if dealer busts with soft hand
@@ -197,7 +197,7 @@ def soft_17(user_list, deck, total):
 	while total < 17:
 		curr_card = deck.pop()
 		user_list[0]['current_hand'].append(curr_card)
-		curr_card_value = DECK_DICT[curr_card]['actual_value'][0]
+		curr_card_value = DECK_DICT[curr_card]['value'][0]
 
 		if curr_card_value in [1, 2, 3, 4] and first_round: # since soft 17 is either 7 or 17, this will make 18 to 21 only in first round
 			total = 17 + curr_card_value
